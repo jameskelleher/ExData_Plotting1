a07d231027cb2f6b3637d0cca8112903cf24f4cc
James Kelleher <jk3047@nyu.edu>
2015-12-13 19:03:55 -0500

    added data for first plot

---
+++ plot1.R
@@ -0,0 +1,12 @@
+library(data.table)
+
+data.all <- fread("household_power_consumption.txt", set=";", na.strings = "?")
+dt <- subset(data.all, Date == "1/2/2007" | Date == "2/2/2007")
+dt$Date <- as.IDate(as.Date(dt$Date, "%m/%d/%Y"))
+dt$Time <- as.ITime(strptime(dt$Time, "%H:%M:%S"))
+
+quartz()
+hist(dt$Global_active_power, col = "red", main = "Global Active Power", xlab = "Global Active Power (kilowatts)")
+dev.copy(png, file = "plot1.png")
+dev.off()
+
